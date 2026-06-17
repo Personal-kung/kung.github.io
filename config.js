4d853c15f624ed69adb024c9d0d3fb811d1498eb
thinkpad <kelvinmext1@gmail.com>
2026-06-18 03:35:16 +0900

    adding JSAM info

---
+++ config.js
@@ -0,0 +1,9 @@
+window.FIREBASE_CONFIG = {
+    apiKey: "AIzaSyDhqDQNw2py0c6-KVKzt2E-cHj2TOCY3hs",
+    authDomain: "personal-portfolio-7cadc.firebaseapp.com",
+    projectId: "personal-portfolio-7cadc",
+    storageBucket: "personal-portfolio-7cadc.firebasestorage.app",
+    messagingSenderId: "684119855739",
+    appId: "1:684119855739:web:01143f994e954ebe5d08de",
+    measurementId: "G-9ZRZ3WY651"
+};
